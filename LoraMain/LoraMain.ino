6d485dedfceaf3d09abecc0a27b186a1ac19e62b
LizOnAir <choo.ke.ying@gmail.com>
2026-08-31 11:25:25 +0800

    Add files via upload

---
+++ LoraMain/LoraMain.ino
@@ -0,0 +1,186 @@
+//main
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <RadioLib.h>
+#include <Adafruit AHTx0>
+
+#define INITIATING_NODE
+
+//LoRa pin
+#define loraSCKpin 46;
+#define loraMISOpin 10;
+#define loraMOSIpin 9;
+#define loraNSSpin 3;
+#define loraRSTpin 11;
+#define loraBUSYpin 12;
+#define loraDIO1pin 13;
+
+//sensor pin
+#define sensorMOSI 9;
+#define sensorSCK 46;
+
+//display pin
+#define displayMOSI 9;
+#define displaySCK 46;
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//NSS, DI01, NRST, BUSY
+SX1262 radio = new Module(loraNSSpin, loraDIO1pin, loraRSTpin, loraBUSYpin);
+
+// save transmission states between loops
+int transmissionState = RADIOLIB_ERR_NONE;
+
+// flag to indicate transmission or reception state
+bool transmitFlag = false;
+
+// flag to indicate that a packet was sent or received
+volatile bool operationDone = false;
+
+#if defined(ESP32)
+  ICACHE_RAM_ATTR
+#endif
+
+void setFlag(void) {
+  // we sent or received a packet, set the flag
+  operationDone = true;
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize SX1262 at 434 MHz
+  Serial.print(F("[SX1262] Initializing ... "));
+  ConfigLoRa_t config;
+  config.frequency = 434;
+  int state = radio.begin(config);
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true) { delay(10); }
+  }
+
+  // set the function that will be called
+  // when new packet is received
+  radio.setDio1Action(setFlag);
+
+  #if defined(INITIATING_NODE)
+    // send the first packet on this node
+    Serial.print(F("[SX1262] Sending first packet ... "));
+    transmissionState = radio.startTransmit(temp.temperature, humidity.relative_humidity);
+    transmitFlag = true;
+  #else
+    // start listening for LoRa packets on this node
+    Serial.print(F("[SX1262] Starting to listen ... "));
+    state = radio.startReceive();
+    if (state == RADIOLIB_ERR_NONE) {
+      Serial.println(F("success!"));
+    } else {
+      Serial.print(F("failed, code "));
+      Serial.println(state);
+      while (true) { delay(10); }
+    }
+  #endif
+
+  if (!aht.begin()) {
+    Serial.println("Could not find AHT");
+    while (1)
+      delay(10);
+  }
+  Serial.println("AHT10 or AHT20 found");
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 30);
+  display.println("Display Init...");
+  display.display();
+  delay(1000);
+
+}
+
+void loop() {
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity,
+               &temp); // populate temp and humidity objects with fresh data
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degrees C");
+  Serial.print("Humidity: ");
+  Serial.print(humidity.relative_humidity);
+  Serial.println("% rH");
+
+  delay(500);
+
+  // check if the previous operation finished
+  if(operationDone) {
+    // reset flag
+    operationDone = false;
+
+    if(transmitFlag) {
+      // the previous operation was transmission, listen for response
+      // print the result
+      if (transmissionState == RADIOLIB_ERR_NONE) {
+        // packet was successfully sent
+        Serial.println(F("transmission finished!"));
+
+      } else {
+        Serial.print(F("failed, code "));
+        Serial.println(transmissionState);
+
+      }
+
+      // listen for response
+      radio.startReceive();
+      transmitFlag = false;
+
+    } else {
+      // the previous operation was reception
+      // print data and send another packet
+      String str;
+      int state = radio.readData(str);
+
+      if (state == RADIOLIB_ERR_NONE) {
+        // packet was successfully received
+        Serial.println(F("[SX1262] Received packet!"));
+
+        // print data of the packet
+        Serial.print(F("[SX1262] Data:\t\t"));
+        Serial.println(str);
+
+        // print RSSI (Received Signal Strength Indicator)
+        Serial.print(F("[SX1262] RSSI:\t\t"));
+        Serial.print(radio.getRSSI());
+        Serial.println(F(" dBm"));
+
+        // print SNR (Signal-to-Noise Ratio)
+        Serial.print(F("[SX1262] SNR:\t\t"));
+        Serial.print(radio.getSNR());
+        Serial.println(F(" dB"));
+      }
+
+      // wait a second before transmitting again
+      delay(10000);
+
+      // send another one
+      Serial.print(F("[SX1262] Sending another packet ... "));
+      transmissionState = radio.startTransmit(temp.temperature, humidity.relative_humidity);
+      transmitFlag = true;
+    }
+  
+  }
+  display.setCursor(0, 20);
+  display.print("Data:");
+  display.setCursor(0, 30);
+  display.print(temp.temperature,humidity.relative_humidity);
+  display.display();
+  render();  
+
+}
